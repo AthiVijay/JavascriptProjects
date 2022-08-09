5d2d3a20d7bf87bcdf666a7cce08aa2ed74aa833
vijay <athi.vijaykumar20@ifheindia.org>
2022-08-09 15:09:56 +0530

    Refactored code note working, had to find the reason

--- DOM PROJECTS/2/index.js
+++ DOM PROJECTS/2/index.js
@@ -1,44 +1,66 @@
-let [element] = document.getElementsByClassName("circle");
-element.setAttribute("draggable", "true");
-element.style.position = "absolute";
+
+function createCircle(){
+  console.log('Entered Here');
+  const item = document.createElement("div");
+  console.log(item);
+  return item;
+}
+
+function setDraggableTrue(element){
+  element.setAttribute('draggable', 'true')
+}
+
+function makeElementPositionAbsolute(element){
+  element.style.position = 'absolute';
+}
 
 function setElementCoordinatesFromEvent(element, event) {
   element.style.left = event.pageX + "px";
   element.style.top = event.pageY + "px";
 }
 
-element.ondrag = function (event) {
-  setElementCoordinatesFromEvent(this, event);
-};
+function makeElementDraggable(element){
 
-element.ondragend = function (event) {
-  setElementCoordinatesFromEvent(this, event);
-};
+  element.ondrag = function (event) {
+    setElementCoordinatesFromEvent(this, event);
+  };
+  
+  element.ondragend = function (event) {
+    setElementCoordinatesFromEvent(this, event);
+  };
+  
+  element.ondrag = function (event) {
+    setElementCoordinatesFromEvent(this, event);
+  };
 
-element.ondrag = function (event) {
-  setElementCoordinatesFromEvent(this, event);
-};
+}
+
+
 
 document.addEventListener('click', function (evt) {
 
   if(evt.detail === 3){
 
-      console.log('Triple click');
-      const item = document.createElement("div");
-      item.classList.add('circle');
-      item.setAttribute("draggable", "true");
-      item.style.position = "absolute";
+    console.log('Triple click happened');
+    const circle = createCircle();
+    console.log('Done One',circle);
+    setDraggableTrue(circle);
+    console.log('Done Two',circle);
 
-      item.ondragend = function (event) {
-        setElementCoordinatesFromEvent(this, event);
-      };
-      
-      item.ondrag = function (event) {
-        setElementCoordinatesFromEvent(this, event);
-      };
+    makeElementPositionAbsolute(circle);
+    // makeElementDraggable(circle);   
 
-      document.body.appendChild(item);
+    circle.ondrag = function (event) {
+      setElementCoordinatesFromEvent(this, event);
+    };
     
+    circle.ondragend = function (event) {
+      setElementCoordinatesFromEvent(this, event);
+    };
+    
+    circle.ondrag = function (event) {
+      setElementCoordinatesFromEvent(this, event);
+    };
     
     }
 
