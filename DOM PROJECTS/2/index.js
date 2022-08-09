e6b794acc4576b533d356b3544ac53148cd2f1cb
vijay <athi.vijaykumar20@ifheindia.org>
2022-08-09 14:42:47 +0530

    Created moderate version of duplicate elements

--- DOM PROJECTS/2/index.js
+++ DOM PROJECTS/2/index.js
@@ -8,16 +8,39 @@ function setElementCoordinatesFromEvent(element, event) {
 }
 
 element.ondrag = function (event) {
-  console.log(this);
   setElementCoordinatesFromEvent(this, event);
 };
 
 element.ondragend = function (event) {
-  console.log(this);
   setElementCoordinatesFromEvent(this, event);
 };
 
 element.ondrag = function (event) {
-  console.log(this);
   setElementCoordinatesFromEvent(this, event);
 };
+
+document.addEventListener('click', function (evt) {
+
+  if(evt.detail === 3){
+
+      console.log('Triple click');
+      const item = document.createElement("div");
+      item.classList.add('circle');
+      item.setAttribute("draggable", "true");
+      item.style.position = "absolute";
+
+      item.ondragend = function (event) {
+        setElementCoordinatesFromEvent(this, event);
+      };
+      
+      item.ondrag = function (event) {
+        setElementCoordinatesFromEvent(this, event);
+      };
+
+      document.body.appendChild(item);
+    
+    
+    }
+
+}
+);
